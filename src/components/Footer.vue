e7a4bdd6da942782676a113aeba421a84d1c2390
WuYu <492444775@qq.com>
2020-07-26 17:20:25 +0800

    初始化系统组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      footer
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Footer",
+      data() {
+        return {
+
+        };
+      },
+      methods: {
+
+      }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      header
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Header",
+      data() {
+        return {
+
+        };
+      },
+      methods: {
+
+      }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
