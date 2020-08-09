a94c5bae2eb32a8a75f407d33e9234bf09f92e82
WuYu <492444775@qq.com>
2020-08-09 15:58:07 +0800

    添加配置

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,19 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  configureWebpack:{
+    performance: {
+      hints:'warning',
+      //入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      //生成文件的最大体积
+      maxAssetSize: 30000000,
+      //只给出 js 文件的性能提示
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith('.js');
+      }
+    }
+  },
   devServer: {
     https: false,
     open: true,
